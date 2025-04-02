649a6b52e5aeac186c4b5990c6cc9d4dfd962f31
Clemens Altrichter <c.altrichter@students.htl-leonding.ac.at>
2025-04-02 12:46:42 +0200

    Library Header

--- project/pages/GUI.js
+++ project/pages/GUI.js
@@ -1,23 +1,29 @@
 function switchLibraryHeader(){
     console.log('switchHeader');
     
-    document.getElementById('header').style.height = '35vh'
+   // document.getElementById('header').style.height = '35vh'
+    document.getElementById('header').style = ' height:max-content; display:flex; flex-direction: column; justify-content: space-evenly;'
 
     document.getElementById('header').innerHTML=`
-    <div id="sortBox">
+    <div id="sortBox" class="headerSettingBox">
 
+    <div class="headerInnerBox">
     <label for="sort">Sort by: </label>
         <select name="sort" id="sortInput">
             <option value="youngest">Youngest</option>
             <option value="oldest">Oldest</option>
             <option value="rating">Rating</option>
         </select>
+    </div>
 
+    <div class="headerInnerBox">
     <label for="nameInput">Search for Name: </label>
         <input type="text" placeholder="Song 1" name="nameInput" id="nameInput">    
 
     </div>
-     <div id="libraryColorBox">
+
+    </div>
+     <div id="libraryColorBox" class="headerSettingBox">
 
      <p>Library Color: </p>
      <div id="colorsContainer">
@@ -27,7 +33,7 @@ function switchLibraryHeader(){
     </div>
 
     </div>
-     <div id="CardBox">
+     <div id="CardBox" class="headerSettingBox">
      <p>Card Color: </p>
      <div id="colorsContainer">
         <div></div>
