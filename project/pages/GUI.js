ae20e0bfb936f8baafe8fa4e866a1926bb79513c
Clemens Altrichter <c.altrichter@students.htl-leonding.ac.at>
2025-04-07 18:56:17 +0200

    grundprinzip der Shop seite festgelegt

--- project/pages/GUI.js
+++ project/pages/GUI.js
@@ -36,11 +36,11 @@ function switchLibraryHeader(){
 
     </div>
      <div id="CardBox" class="headerSettingBox">
-     <p>Card Color: </p>
+     <p>Card Background: </p>
      <div id="colorsContainer">
-       <div class="cardCol" id="cardCol_blue"></div>
-        <div class="cardCol" id="cardCol_red"></div>
-        <div class="cardCol" id="cardCol_grey"></div>
+       <div class="cardCol" id="cardCol_blue" onclick="changeCardBackground('#0000b3','#000099')"></div>
+        <div class="cardCol" id="cardCol_red" onclick="changeCardBackground('#b30000','#800000')"></div>
+        <div class="cardCol" id="cardCol_grey" onclick="changeCardBackground('#29293f','#29292f')"></div>
     </div>
 
     <p>Rounded Edges: </p>
@@ -53,14 +53,42 @@ function switchLibraryHeader(){
     </div>
     </div>
 
-    <div id="finishHeaderBTN">
+    <div id="finishHeaderBTN" onclick="returnLibraryHeader()">
     <p>Finish</p>
     </div>
 
     `
 }
 
+function returnLibraryHeader(){
+
+    document.getElementById('header').style = ' height:25vh;'
+
+    document.getElementById('header').innerHTML=`
+
+    <a href="../index.html">
+    <!-- <div class="returnBTN"> -->
+    <div class="returnContainer">
+     <svg id="polygon1" xmlns="http://www.w3.org/2000/svg" width="50" height="60" viewBox="0 0 68 78" fill="none">
+         <path d="M0 39L67.5 0.0288544V77.9711L0 39Z" fill="#EBEAE5" />
+       </svg>
+     <svg id="polygon2" xmlns="http://www.w3.org/2000/svg" width="60" height="70
+     " viewBox="0 0 75 88" fill="none">
+         <path d="M74.5 86.4352L1 44L74.5 1.56476V86.4352Z" fill="#EBEAE5" stroke="black"/>
+       </svg>
+ </div></a>
+
+ <h1 onclick="logging()">Library</h1>
+
+ <img src="../img/vinyl.png" alt="settings" id="settingsVinyl" onclick="switchLibraryHeader()">
+`
+ }
+
 function changeBorderRadius(val){
-    this.style = 'background-color: white; color; black;'
-root.style = `--borderRadius: ${val}px`
+    this.style = 'background-color: white; color: black;'
+root.style = `--borderRadius: ${val}px;`
+}
+
+function changeCardBackground(val1,val2){
+root.style = `--cardBackground: linear-gradient( ${val1}, ${val2});`
 }
--- project/verlinkung.js
+++ project/verlinkung.js
@@ -7,6 +7,9 @@ window.location.replace('./pages/tutorial.html')
 function toLibrary(){
     window.location.replace('./pages/library.html') 
 }
+function toShop(){
+    window.location.replace('./pages/shop.html') 
+}
 
 
 
