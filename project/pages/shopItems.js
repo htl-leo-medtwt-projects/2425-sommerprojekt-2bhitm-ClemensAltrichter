e3937d020e045f693b5721cf30954e6f392cf3f1
Clemens Altrichter <c.altrichter@students.htl-leonding.ac.at>
2025-05-30 17:47:10 +0200

    Listitems

--- project/pages/shopItems.js
+++ project/pages/shopItems.js
@@ -1,9 +1,20 @@
 console.log("Itemd");
 
 
+
+
+
+  
+  
+  
+  
+  
+  
+  'Tambourine'
+  'Tambourine1'
 let shopItems = [
         {
-            "title": "Bass1",
+            "title": 'Kick',
             "color": "gold",
             "price": 350,
             "link":"",
@@ -11,7 +22,7 @@ let shopItems = [
             "buyable":true
         },
         {
-            "title": "Clap",
+            "title": 'hiHat1',
             "color": "yellow",
             "price": 500,
             "link":"",
@@ -19,7 +30,7 @@ let shopItems = [
             "buyable":true
         },
         {
-            "title": "808",
+            "title": 'hiHat2',
             "color": "white",
             "price": 130,
             "link":"",
@@ -27,7 +38,7 @@ let shopItems = [
             "buyable":true
         },
         {
-            "title": "Synth",
+            "title": 'Maracas',
             "color": "red", 
             "price": 210,
             "link":"",
@@ -35,17 +46,129 @@ let shopItems = [
             "buyable":true
         },
         {
-            "title": "Drum",
+            "title": 'Rim',
             "color": "azure",
-            "price": 75,
+            "price": 1000,
             "link":"",
             "cover":"../img/Instrument-Icon.png",
             "buyable":true
         },
         {
-            "title": "Snare",
+            "title": 'Snare1',
             "color": "gold",
-            "price": 1500,
+            "price": 400,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        }, 
+        {
+            "title": 'Conga',
+            "color": "red",
+            "price": 500,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+         {
+            "title": 'hiHat3',
+            "color": "gold",
+            "price": 360,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+         {
+            "title": 'hiHat4',
+            "color": "wheat",
+            "price": 150,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+         {
+            "title": 'hiHat5',
+            "color": "azure",
+            "price": 100,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+         {
+            "title": 'hiHat6',
+            "color": "darkred",
+            "price": 720,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+         {
+            "title": 'Kick1',
+            "color": "smoke",
+            "price": 400,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+        {
+            "title": 'Kick2',
+            "color": "pink",
+            "price": 250,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+        {
+            "title": 'Kick3',
+            "color": "orange",
+            "price": 250,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+        {
+            "title": 'Kick4',
+            "color": "red",
+            "price": 600,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+        {
+            "title": 'Snare2',
+            "color": "azure",
+            "price": 175,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+        {
+            "title": 'Snare3',
+            "color": "gold",
+            "price": 750,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+        {
+            "title": 'Snare4',
+            "color": "wheat",
+            "price": 99,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+        {
+            "title": 'Tambourine',
+            "color": "yellow",
+            "price": 200,
+            "link":"",
+            "cover":"../img/Instrument-Icon.png",
+            "buyable":true
+        },
+        {
+            "title": 'Tambourine1',
+            "color": "azure",
+            "price": 375,
             "link":"",
             "cover":"../img/Instrument-Icon.png",
             "buyable":true
